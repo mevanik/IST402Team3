b83d9420caac390912e3b6512dfed522d52c75cd
wxp13 <wxp13@psu.edu>
2021-04-23 19:28:24 -0400

    edited app.py

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/reminder/")
+@app.route("/")
 def home():
     return render_template("reminder.html")
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
+
+if __name__ == '__main__':
+    # Run the app server on localhost:5900
+    app.run('localhost', 5900)
